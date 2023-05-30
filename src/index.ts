d9dd85dc4218a1bed8fb37821b2d6018efe52cfe
Alessandro Delass <soudasuwa@proton.me>
2023-05-30 15:28:07 +0300

    Use new typings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import type {
+	InlineResponse20012, FarmListItem,
+	InlineResponse20021, WorkerListItem,
+	Farm,
+	InlineResponse20028, WorkerMetric
+} from './hive'
+
 import axios, { AxiosError, AxiosInstance } from 'axios'
 
 const API_ROOT = 'https://api2.hiveos.farm/api/v2/'
@@ -9,286 +16,6 @@ export interface HiveOSAPIErrorData {
 	message: HiveOSAPIErrorMessage
 	redirect: string
 }
-
-export interface FarmsGet {
-	data: Farm[]
-	tags: Tag[]
-}
-
-export interface MetricsGet {
-	data: Metric[]
-}
-
-/**
- * @interface Metric
- */
-export interface Metric {
-	/**
-	 * @type {number}
-	 * @memberof Metric
-	 */
-	time?: number
-
-	/**
-	 * GPUs or ASIC boards count
-	 * @type {number}
-	 * @memberof Metric
-	 */
-	units?: number
-
-	/**
-	 * Temperature, °C
-	 * @type {number[]}
-	 * @memberof Metric
-	 */
-	temp?: number[]
-
-	/**
-	 * Fan speed
-	 * @type {number[]}
-	 * @memberof Metric
-	 */
-	fan?: number[]
-
-	/**
-	 * Total power draw, watts
-	 * @type {number}
-	 * @memberof Metric
-	 */
-	power?: number
-
-	/**
-	 * Hashrates by algorithm
-	 * @type {Hashrates[]}
-	 * @memberof Metric
-	 */
-	hashrates?: Hashrates[]
-
-	/**
-	 * @type {FanflapStats}
-	 * @memberof Metric
-	 */
-	fanflap?: FanflapStats
-
-	/**
-	 * @type {PowermeterStats}
-	 * @memberof Metric
-	 */
-	powermeter?: PowermeterStats
-}
-
-/**
- * Powermeter controller stats. Each value is an array containing values from corresponding meter. 
- * @interface PowermeterStats
- */
-export interface PowermeterStats {
-	/**
-	 * Current power draw, kilowatts (kW)
-	 * @type {Array<number>[]}
-	 * @memberof PowermeterStats
-	 */
-	power?: Array<number>[]
-
-	/**
-	 * Current total power draw, kilowatts (kW)
-	 * @type {number[]}
-	 * @memberof PowermeterStats
-	 */
-	powerTotal?: number[]
-
-	/**
-	 * Power usage value, kilowatthours (kWh)
-	 * @type {number[]}
-	 * @memberof PowermeterStats
-	 */
-	energyTotal?: number[]
-}
-
-/**
- * FanFlap controller stats
- * @interface FanflapStats
- */
-export interface FanflapStats {
-	/**
-	 * Fan speeds, %
-	 * @type {number[]}
-	 * @memberof FanflapStats
-	 */
-	fan?: number[]
-}
-
-/**
- * @interface Hashrates
- */
-export interface Hashrates {
-	/**
-	 * @type {AlgoName}
-	 * @memberof Hashrates
-	 */
-	algo?: string
-
-	/**
-	 * Hashrate
-	 * @type {number[]}
-	 * @memberof Hashrates
-	 */
-	values?: number[]
-}
-
-export interface Farm {
-	id: number
-	name: string
-	timezone: string
-	nonfree: boolean
-	twofa_required: boolean
-	trusted: boolean
-	gpu_red_temp: number
-	asic_red_temp: number
-	gpu_red_fan: number
-	asic_red_fan: number
-	gpu_red_asr: number
-	asic_red_asr: number
-	gpu_red_la: number
-	asic_red_la: number
-	gpu_red_cpu_temp: number
-	gpu_red_mem_temp: number
-	asic_red_board_temp: number
-	autocreate_hash: string
-	locked: boolean
-	tag_ids: number[]
-	auto_tags: boolean
-	workers_count: number
-	rigs_count: number
-	asics_count: number
-	disabled_rigs_count: number
-	disabled_asics_count: number
-	owner: Owner
-	money: Money
-	stats: Stats
-	hashrates?: Hashrate[]
-	hashrates_by_coin?: HashratesByCoin[]
-	personal_settings?: PersonalSettings
-	charge_on_pool: boolean
-	role?: string
-	power_price?: number
-	power_price_currency?: string
-	psu_efficiency?: number
-	default_fs?: DefaultFs
-	payer?: Payer
-	hardware_power_draw?: number
-}
-
-export interface Owner {
-	id: number
-	login: string
-	name?: string
-	me?: boolean
-}
-
-export interface Money {
-	is_paid: boolean
-	is_free: boolean
-	paid_cause?: string
-	balance: number
-	discount: number
-	daily_cost: number
-	monthly_cost: number
-	overdraft: boolean
-	cost_details?: CostDetail[]
-	daily_price: number
-	monthly_price: number
-	daily_use_rigs: number
-	daily_use_asics: number
-	price_per_rig: number
-	price_per_asic: number
-	overdraft_days_left?: number
-	days_left?: number
-}
-
-export interface CostDetail {
-	type: number
-	type_name: string
-	amount: number
-	monthly_price: number
-	monthly_cost: number
-	daily_cost: number
-}
-
-export interface Stats {
-	workers_total: number
-	workers_online: number
-	workers_offline: number
-	workers_overheated: number
-	workers_no_temp: number
-	workers_no_fan: number
-	workers_overloaded: number
-	workers_invalid: number
-	workers_low_asr: number
-	workers_no_hashrate: number
-	workers_with_problem: number
-	rigs_total: number
-	rigs_online: number
-	rigs_offline: number
-	rigs_power: number
-	gpus_total: number
-	gpus_online: number
-	gpus_offline: number
-	gpus_overheated: number
-	gpus_no_temp: number
-	asics_total: number
-	asics_online: number
-	asics_offline: number
-	asics_power: number
-	boards_total: number
-	boards_online: number
-	boards_offline: number
-	boards_overheated: number
-	boards_no_temp: number
-	cpus_online: number
-	devices_total: number
-	devices_online: number
-	devices_offline: number
-	power_draw: number
-	asr?: number
-	power_cost?: number
-}
-
-export interface Hashrate {
-	algo: string
-	hashrate: number
-}
-
-export interface HashratesByCoin {
-	coin: string
-	algo: string
-	hashrate: number
-}
-
-export interface PersonalSettings {
-	is_favorite: boolean
-}
-
-export interface DefaultFs {
-	"1"?: number
-	"2"?: number
-}
-
-export interface Payer {
-	id: number
-	login: string
-	name: string
-}
-
-export interface Tag {
-	id: number
-	type_id: number
-	user_id: number
-	name: string
-	color: number
-	is_auto: boolean
-	farms_count: number
-}
-
 
 export class HiveOSAPIError extends Error {
 	constructor({ code, message }: HiveOSAPIErrorData) {
@@ -326,17 +53,22 @@ export class HiveOSAPI {
 		}
 	}
 
-	async farms(): Promise<Farm[]> {
-		return this.request<FarmsGet>(`/farms`)
-			.then(result => result.data)
+	async farms(): Promise<FarmListItem[]> {
+		return this.request<InlineResponse20012>(`/farms`)
+			.then(result => result.data || [])
 	}
 
 	async farm(id: number): Promise<Farm> {
 		return this.request<Farm>(`/farms/${id}`)
 	}
 
-	async metrics(farm: number, worker: number): Promise<Metric[]> {
-		return this.request<MetricsGet>(`/farms/${farm}/workers/${worker}/metrics`)
-			.then(result => result.data)
+	async workers(farm: number): Promise<WorkerListItem[]> {
+		return this.request<InlineResponse20021>(`/farms/${farm}/workers`)
+			.then(result => result.data || [])
+	}
+
+	async metrics(farm: number, worker: number): Promise<WorkerMetric[]> {
+		return this.request<InlineResponse20028>(`/farms/${farm}/workers/${worker}/metrics`)
+			.then(result => result.data || [])
 	}
 }
