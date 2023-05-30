00e76f6df03ab36414d680c9a04e97af75dd25e7
Alessandro Delass <soudasuwa@proton.me>
2023-05-30 13:05:44 +0300

    Set default `method` to `GET`

--- src/index.ts
+++ src/index.ts
@@ -188,7 +188,7 @@ export class HiveOSAPI {
 		})
 	}
 
-	async request<T>(url: string = '/', method?: string): Promise<T> {
+	async request<T>(url: string = '/', method: string = 'GET'): Promise<T> {
 		try {
 			const response = await this.apiClient.request<T>({
 				method: method,
